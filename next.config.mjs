f73edbd476ede01783169abcb818016805a77bd5
Lautaro Romero Temperini <latta.romero@gmail.com>
2025-02-25 06:20:18 -0300

    Primer commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Exporta la app como estática
-    basePath: "/xvvave.github.io", // Nombre del repo en GitHub (cambia esto)
+    basePath: "/xvvave", // Nombre del repo en GitHub (cambia esto)
     assetPrefix: "/xvvave.github.io/",
   };
   
